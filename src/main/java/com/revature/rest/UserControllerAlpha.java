438561e19f87a17cb0aa5e6379fff29ee2907194
Gibby Hart <gibbidy_5@hotmail.com>
2019-05-04 14:03:02 -0400

    Endpoint testing for registering a new user works

--- src/main/java/com/revature/rest/UserControllerAlpha.java
+++ src/main/java/com/revature/rest/UserControllerAlpha.java
@@ -5,9 +5,12 @@ import static com.revature.util.ClientMessageUtil.SOMETHING_WENT_WRONG;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.revature.ajax.ClientMessage;
@@ -15,7 +18,8 @@ import com.revature.model.User;
 import com.revature.service.UserService;
 
 @Controller("userController")
-
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path="/user", produces= MediaType.APPLICATION_JSON_VALUE)
 public class UserControllerAlpha implements UserController{
 
 	private static Logger logger = Logger.getLogger(UserControllerAlpha.class);
