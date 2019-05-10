6ad724b9ebeccd87f1c270aca23d58d5dd711de7
Gibby Hart <gibbidy_5@hotmail.com>
2019-05-10 16:28:46 -0400

    Refactored the view dispatcher to be hard coded to find the index.html page to resolve issue of said page not loading. Removed unnessicary jdbc drivers and refactored packages to clean up directory

--- src/main/java/com/revature/rest/TopItemsToConsumeController.java
+++ src/main/java/com/revature/rest/TopItemsToConsumeController.java
@@ -1,15 +1,14 @@
 package com.revature.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 @CrossOrigin
 @RequestMapping("/api/topItemsToConsume")
 public class TopItemsToConsumeController {
--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -1,13 +1,19 @@
-//package com.revature.service;
-//
-//import java.util.List;
-//
-//import com.revature.model.User;
-//
-//public interface UserService {
-//
-//	public boolean registerUser(User user);
-//	public List<User> getAllUsers();
-//	public User getUser(String name);
-//
-//}
+package com.revature.service;
+
+import com.revature.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by evelan on 12/21/15.
+ */
+public interface UserService extends UserDetailsService {
+
+    User findByEmail(String email);
+
+    User register(User user);
+
+    User findByName(String name);
+
+    boolean registerUser(User user);
+
+}
